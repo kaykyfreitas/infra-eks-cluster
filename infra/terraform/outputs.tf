ef3d6c601dc21bfbd45f3892060e1d2969dc815e
Kayky Freitas <kayky_defreitas@hotmail.com>
2025-11-03 02:05:20 -0300

    feat: add main terraform

---
+++ infra/terraform/outputs.tf
@@ -0,0 +1,4 @@
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+}
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,65 @@
+variable "backend_bucket_name" {
+  description = "Terraform backend AWS bucket name"
+  type = string
+}
+
+variable "repository_name" {
+  description = "Git repository name"
+  type = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "Public subnet CIDR block"
+  type        = list(string)
+}
+
+variable "private_subnet_cidr" {
+  description = "Private subnet CIDR block"
+  type        = list(string)
+}
+
+variable "availability_zone" {
+  description = "Availability zones"
+  type        = list(string)
+}
+
+variable "eks_cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "EKS cluster version"
+  type        = string
+}
+
+variable "node_groups" {
+  description = "EKS cluster node group config"
+  type = map(
+    object({
+      instance_types = list(string)
+      capacity       = string
+
+      scaling_config = object({
+        desired_size = number
+        max_size     = number
+        min_size     = number
+      })
+    })
+  )
+}
