1a7c606bada3eb06ed5252ac9791fdfed5ab0594
Kayky Freitas <kayky_defreitas@hotmail.com>
2025-11-22 15:56:46 -0300

    fix: remove iam outputs

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -1,7 +1,3 @@
-############################################################
-# IAM USER PARA ACESSO AO EKS
-############################################################
-
 resource "aws_iam_user" "eks_admin_user" {
   name = "eks-admin-user"
   tags = {
@@ -13,10 +9,6 @@ resource "aws_iam_user" "eks_admin_user" {
 resource "aws_iam_access_key" "eks_admin_key" {
   user = aws_iam_user.eks_admin_user.name
 }
-
-############################################################
-# ROLE IAM ADMIN PARA ASSUMIR PERMISSÕES DE ADMIN EKS
-############################################################
 
 resource "aws_iam_role" "eks_admin_role" {
   name = "eks-admin-role"
@@ -40,37 +32,7 @@ resource "aws_iam_role" "eks_admin_role" {
   }
 }
 
-############################################################
-# PERMISSÕES ADMINISTRATIVAS (AWS) PARA ESSA ROLE
-############################################################
-
 resource "aws_iam_role_policy_attachment" "eks_admin_role_admin_access" {
   role       = aws_iam_role.eks_admin_role.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
-############################################################
-# OUTPUTS ÚTEIS
-############################################################
-
-output "eks_admin_user_arn" {
-  description = "ARN do usuário EKS admin"
-  value       = aws_iam_user.eks_admin_user.arn
-}
-
-output "eks_admin_role_arn" {
-  description = "ARN da role EKS admin"
-  value       = aws_iam_role.eks_admin_role.arn
-}
-
-output "eks_admin_user_access_key" {
-  description = "Access key para o usuário admin"
-  value       = aws_iam_access_key.eks_admin_key.id
-  sensitive   = true
-}
-
-output "eks_admin_user_secret_key" {
-  description = "Secret key para o usuário admin"
-  value       = aws_iam_access_key.eks_admin_key.secret
-  sensitive   = true
 }
